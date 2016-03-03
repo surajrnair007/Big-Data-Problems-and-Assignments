3b28f5d3b617957587c3aafc5e053e6861af6a09
surajrnair007 <surajrnair007@gmail.com>
2016-03-03 14:58:14 +0530

    This program has to receive the input file as an argument. Not tested

--- A1Prob1MapperV1.1.py
+++ A1Prob1MapperV1.1.py
@@ -11,7 +11,7 @@ This program does the following:-
 """
 
 
-#Defining a dictionary containing each letter of the alphabet
+#Defining a dictionary containing each letter of the alphabet 
 
 alphachar = { "a":0,  "b":1 , "c":2 , "d":3 , "e":4 , "f":5 , "g":6 , "h":7 ,
               "i":8,  "j":9 , "k":10, "l":11, "m":12, "n":13, "o":14, "p":15,
